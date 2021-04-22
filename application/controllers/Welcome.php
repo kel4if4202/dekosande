e480c09e9c9bb4162c79961015dca58fc0d71c04
dimasguintana <dguint@outlook.com>
2021-04-22 17:32:33 +0700

    view

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,17 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+	class Controllername extends CI_Controller {
 
-	public function index()
-	{
-		$this->load->view('welcome_message');
+		public function __construct()
+		{
+			parent::__construct();
+			$this->load->helper('form');
+		}
+
+		public function index()
+		{
+			session_destroy();
+			$content['main_view'] = 'LoginView';
+			$content['title'] = 'Login';
+			$this->load->view('PageView', $content);
+		}
+
 	}
-
-	public function home()
-	{
-		$this->load->view('home');
-		
-	}
-
-}
