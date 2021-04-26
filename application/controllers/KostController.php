c59e60b089a62b390170f53f8716ecb4ba3747e4
Muhammad Zaky <mhmdzakyy10@gmail.com>
2021-04-26 09:09:50 +0700

    add view kost

---
+++ application/controllers/KostController.php
@@ -0,0 +1,33 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class KostController extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->model('KostModel');
+        
+        
+    }
+
+    public function index()
+    {
+        $data['main_view'] = 'ListKosView';
+        $data['title'] = 'Kost View';
+        $this->load->view('PageView', $data);
+    }
+
+    public function data_kost()
+    {
+        $data = $this->KostModel->GetKost();
+
+        echo json_encode($data);
+    }
+
+}
+
+/* End of file Controllername.php */
+
--- application/views/PageView.php
+++ application/views/PageView.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/sweetalert2@7.26.10/dist/sweetalert2.min.css" >
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.26.10/dist/sweetalert2.all.min.js" ></script>
-    <link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
+    <!-- <link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css"> -->
+    <link href="https:////cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel="stylesheet">
     <!-- <link rel="icon" href="assets/logo-2.jpg"> -->
@@ -15,7 +16,8 @@
     <script src="http://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.js"></script>
+    <!-- <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.js"></script> -->
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"> </script>
 </head>
 
 <body>
