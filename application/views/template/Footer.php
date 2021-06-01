9189854cf6f7366d871967c2c76b02255b25f15a
Muhammad Zaky <mhmdzakyy10@gmail.com>
2021-06-01 22:23:02 +0700

    update HomeView dan KosView

---
+++ application/views/template/Footer.php
@@ -0,0 +1,6 @@
+<div>
+<!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container"><p class="m-0 text-center text-white">Copyright &copy; DEKOSANDE</p></div>
+    </footer>
+</div>
