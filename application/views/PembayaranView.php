1139c58f4ee22a6f5501bc3f84848b07b7a2cc2e
dimasguintana <dguint@outlook.com>
2021-06-10 14:04:07 +0700

    pembayaran view

--- application/views/PembayaranView.php
+++ application/views/PembayaranView.php
@@ -1,1 +1,28 @@
-<!-- Dimas -->
+<div class="container mt-5 mb-5 d-flex justify-content-center">
+    <div class="card p-5">
+        <div>
+            <h4 class="heading">Upgrade your plan</h4>
+            <p class="text">Please make the payment to start enjoying all the features of our premium plan as soon as possible</p>
+        </div>
+        <div class="pricing p-3 rounded mt-4 d-flex justify-content-between">
+            <div class="images d-flex flex-row align-items-center"> <img class="d-block w-50" src="<?= base_url('asset/0.jpg') ?>">
+                <div class="d-flex flex-column ml-4"> <span class="business">Small Business</span> <span class="plan">CHANGE PLAN</span> </div>
+            </div>
+            <!--pricing table-->
+            <div class="d-flex flex-row align-items-center"> <sup class="dollar font-weight-bold">$</sup> <span class="amount ml-1 mr-1">8,350</span> <span class="year font-weight-bold">/ year</span> </div> <!-- /pricing table-->
+        </div> <span class="detail mt-5">Payment details</span>
+        <div class="credit rounded mt-4 d-flex justify-content-between align-items-center">
+            <div class="d-flex flex-row align-items-center"> <img src="https://i.imgur.com/qHX7vY1.png" class="rounded" width="70">
+                <div class="d-flex flex-column ml-3"> <span class="business">Credit Card</span> <span class="plan">1234 XXXX XXXX 2570</span> </div>
+            </div>
+            <div> <input type="text" class="form-control cvv" placeholder="CVC"> </div>
+        </div>
+        <div class="credit rounded mt-2 d-flex justify-content-between align-items-center">
+            <div class="d-flex flex-row align-items-center"> <img src="https://i.imgur.com/qHX7vY1.png" class="rounded" width="70">
+                <div class="d-flex flex-column ml-3"> <span class="business">Credit Card</span> <span class="plan">2344 XXXX XXXX 8880</span> </div>
+            </div>
+            <div> <input type="text" class="form-control cvv" placeholder="CVC"> </div>
+        </div>
+        <div class="mt-3"> <button class="btn btn-primary btn-block payment-button">Proceed to payment <i class="fa fa-long-arrow-right"></i></button> </div>
+    </div>
+</div>
