68561a80e6538b9848dfc2f6f8336eda433216bb
Rendi Kustiawan <rendikstwn27992gmail.com>
2021-06-11 17:44:10 +0700

    benerin tampilan login dan register

--- application/views/PageView.php
+++ application/views/PageView.php
@@ -17,6 +17,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="<?= base_url('asset/css/styles.css') ?>" rel="stylesheet" />
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../../asset/css/sb-admin-2.min.css" rel="stylesheet">
+
     <title><?= $title ?></title>
 
     <script src="http://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
--- application/views/RegisterView.php
+++ application/views/RegisterView.php
@@ -1,54 +1,83 @@
 <!-- Dimas -->
 <?= $this->session->flashdata('message'); ?>
-<div class="container-fluid ">
-    <div class="row">
-        <div style="height: 90vh; width: 100%" class="d-flex align-items-stretch">
-            <div class="p-3 mb-2 text-black col-6 d-flex align-items-center justify-content-center flex-column">
-                <h1 style=" color: #FF4F5A; font-size: 50;
-        -webkit-text-stroke-width: 0.5px;
-        -webkit-text-stroke-color: black;">Register</h1>
-                <form class='p-5' method="POST" action="<?= site_url('UserController/tambahUser') ?>" style='background-color: #FF4F5A; border-radius: 25px'>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <input type="text" class="form-control" name="no_hp" placeholder="Nomor HP">
+<div class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            <form class="user" method="POST" action="<?= site_url('UserController/tambahUser') ?>">
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" class="form-control form-control-user" name="no_hp"
+                                            placeholder="No Handphone">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user" name="password"
+                                            placeholder="Password">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" name="nik"
+                                        placeholder="NIK">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" name="nama" placeholder="Nama">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" name="email" placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <input type="date" class="form-control form-control-user" name="tgl_lahir" placeholder="YYYY-MM-DD">
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <select value="Pencari" name="peran" class="form-control" >
+                                            <option disabled selected>--Peran--</option>
+                                            <option value="Pencari">Pencari</option>
+                                            <option value="Penjual">Penjual</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <select name="jeniskelamin" class="form-control">
+                                            <option disabled selected>--Jenis Kelamin--</option>
+                                            <option value="Laki-Laki">Laki-Laki</option>
+                                            <option value="Perempuan">Perempuan</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <button type="submit" href="<?= site_url('Welcome/login') ?>" class="btn btn-primary btn-user btn-block">
+                                    Register Account
+                                </button>
+                                <hr>
+                            </form>
+                            <div class="text-center">
+                                <a class="small" href="<?= site_url('Welcome/login') ?>">Already have an account? Login!</a>
+                            </div>
                         </div>
-                        <div class="form-group col-md-6">
-                            <input type="password" class="form-control" name="password" placeholder="Password">
-                        </div>
                     </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="nik" placeholder="NIK">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="nama" placeholder="Nama">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="email" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                        <input type="date" class="form-control" name="tgl_lahir" placeholder="YYYY-MM-DD">
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <select name="peran" class="form-control">
-                                <option disabled selected>--Peran--</option>
-                                <option value="Pencari">Pencari</option>
-                                <option value="Penjual">Penjual</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <select name="jeniskelamin" class="form-control">
-                                <option disabled selected>--Jenis Kelamin--</option>
-                                <option value="Laki-Laki">Laki-Laki</option>
-                                <option value="Perempuan">Perempuan</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <button type="submit" class="btn btn-light mr-auto" id="register" href="<?= site_url('Welcome/login') ?>">Register</button>
-                        <a class='text-light' href="<?= site_url('Welcome/login') ?>">Login</a>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
+
     </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</div>
